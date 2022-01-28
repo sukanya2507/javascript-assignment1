2c0ecc0c32cf40901b80800d9c01a164d27d0881
sukanya2507 <69866254+sukanya2507@users.noreply.github.com>
2022-01-28 13:18:01 +0530

    Add files via upload

---
+++ ques2.js
@@ -0,0 +1,7 @@
+
+const base = prompt(' base of the triangle: ');
+const height = prompt('height of the triangle: ');
+const areaValue = (base * height) / 2;
+console.log(
+  `The area of the triangle is ${areaValue}`
+);
