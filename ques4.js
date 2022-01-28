22287df82e9861214f8b4cdedb02f2ff095170df
sukanya2507 <69866254+sukanya2507@users.noreply.github.com>
2022-01-28 13:23:21 +0530

    Add files via upload

---
+++ ques4.js
@@ -0,0 +1,13 @@
+const number = parseInt(prompt("Enter a number: "));
+
+if (number > 0) {
+    console.log("It's a positive number");
+}
+
+else if (number == 0) {
+  console.log("The number is zero");
+}
+
+else {
+     console.log("It's a negative number");
+}
