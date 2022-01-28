25fb33261879c9e5b618cd012247394e343dabbb
sukanya2507 <69866254+sukanya2507@users.noreply.github.com>
2022-01-28 13:17:00 +0530

    Add files via upload

---
+++ ques1.js
@@ -0,0 +1,6 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
